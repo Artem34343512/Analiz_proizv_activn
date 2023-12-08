990783c8fc45f9aab7b38a914460a35e8421bc66
Artem34343512 <132356263+Artem34343512@users.noreply.github.com>
2023-12-08 10:32:10 +1000

    диаграмма рассеивания

---
+++ diagrras.py
@@ -0,0 +1,20 @@
+from dash import Dash, dcc, html
+import plotly.express as px
+import pandas as pd
+
+df= pd.read_csv('https://raw.githubusercontent.com/Artem34343512/Analiz_proizv_activn/main/productionactivity.csv')
+
+grouped_df = df.groupby(['Дата',]).sum()
+
+df_long = grouped_df.reset_index().melt(id_vars=['Дата'], value_vars=grouped_df.columns[1:],  value_name='Количество_шт')
+
+fig = px.scatter(df_long, x='Дата', y='Количество_шт')
+
+app = Dash(__name__)
+
+app.layout = html.Div([
+ dcc.Graph(figure=fig)
+])
+
+if __name__ == '__main__':
+ app.run_server(debug=True)
